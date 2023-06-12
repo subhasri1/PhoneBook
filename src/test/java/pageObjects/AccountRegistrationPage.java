da988131179cae6cdba8f30eac3401c2fb64544d
Raj <singhabhishekraj581@gmail.com>
2023-06-12 09:56:56 +0530

    opencart added

---
+++ src/test/java/pageObjects/AccountRegistrationPage.java
@@ -0,0 +1,105 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountRegistrationPage extends BasePage {
+
+	public AccountRegistrationPage(WebDriver driver) {
+		super(driver);
+	}
+
+	// Elements
+		@FindBy(name = "firstname")
+		WebElement txtFirstname;
+
+		@FindBy(name = "lastname")
+		WebElement txtLasttname;
+
+		@FindBy(name = "email")
+		WebElement txtEmail;
+
+		@FindBy(name = "telephone")
+		WebElement txtTelephone;
+
+		@FindBy(id = "input-password")
+		WebElement txtPassword;
+
+		@FindBy(name = "confirm")
+		WebElement txtConfirmPassword;
+
+		@FindBy(name = "agree")
+		WebElement chkdPolicy;
+
+		@FindBy(xpath = "//button[@type='submit']")
+		WebElement btnContinue;
+
+		@FindBy(xpath = "//h1[normalize-space()='Your Account Has Been Created!']")
+		WebElement msgConfirmation;
+		@FindBy(xpath= "//input[@name='agree']")
+		WebElement agree;
+
+		public void setFirstName(String fname) {
+			txtFirstname.sendKeys(fname);
+
+		}
+
+		public void setLastName(String lname) {
+			txtLasttname.sendKeys(lname);
+
+		}
+
+		public void setEmail(String email) {
+			txtEmail.sendKeys(email);
+
+		}
+
+		
+		  public void setTelephone(String tel) { 
+			  txtTelephone.sendKeys(tel);
+		  
+		  }
+		 
+
+		public void setPassword(String pwd) {
+			txtPassword.sendKeys(pwd);
+
+		}
+
+		
+		  public void setConfirmPassword(String pwd) {
+		  txtConfirmPassword.sendKeys(pwd);
+		  
+		  }
+		 
+
+		
+		  public void setPrivacyPolicy() { 
+			  chkdPolicy.click();
+		  
+		  }
+		 
+		public void clickAgree()
+		{
+			agree.click();
+		}
+
+		public void clickContinue() {
+			btnContinue.click();
+
+		}
+
+		public String getConfirmationMsg() {
+			try {
+				return (msgConfirmation.getText());
+			} catch (Exception e) {
+				return (e.getMessage());
+
+			}
+
+		}
+		
+
+		
+}
---
+++ src/test/java/testCases/TC_005_AddToCartPageTest.java
@@ -0,0 +1,43 @@
+package testCases;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.SearchPage;
+import testBase.BaseClass;
+
+public class TC_005_AddToCartPageTest extends BaseClass {
+	
+	@Test
+	public void test_addToCart() throws InterruptedException {
+		logger.info(" Starting TC_005_AddToCartPageTest ");
+
+		try {
+			
+			HomePage hp=new HomePage(driver);
+			
+			hp.enterProductName("iPhone");
+			hp.clickSearch();
+			
+			SearchPage sp=new SearchPage(driver);
+			
+			if(sp.isProductExist("iPhone"))
+			{
+				sp.selectProduct("iPhone");
+				sp.setQuantity("2");
+				sp.addToCart();
+				
+			}
+			AssertJUnit.assertEquals(sp.checkConfMsg(),true);
+
+		} catch (Exception e) {
+			Assert.fail();
+		}
+
+		logger.info(" Finished TC_004_SearchProductTest ");
+
+	}
+}
